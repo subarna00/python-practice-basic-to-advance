14779f2a4a36f76f9ae4b847b33e4017c31658f3
pro <pro@pros-MacBook-Pro.local>
2024-06-29 21:15:56 +0545

    quiz

---
+++ python mini projects/quiz_game.py
@@ -0,0 +1,30 @@
+print("Welcome to computer quix")
+
+playing = input("Do you want to play\n >> ")
+
+if playing.lower() != "yes":
+    quit()
+playing = "yes"
+score = 0
+while playing.lower() == "yes":
+    print("==================================================")
+
+    print("Let's play :)")
+
+    answer = input("What does CPU stands for?\n >>")
+    if answer.lower() == "cpu":
+        print("Correct!") 
+        score += 1
+    else:
+        print("Incorrect!")
+
+    answer = input("What does P stands for?\n >>")
+    if answer.lower() == "p":
+        print("Correct!") 
+        score += 1
+    else:
+        print("Incorrect!")
+    
+    print("You scored:" + str(score))
+    score = 0
+    playing = input("Do you want to play again?\n >> ")
